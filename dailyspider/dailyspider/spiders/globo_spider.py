132fca4d67b067a058f932f7142cbe8be26c6c2f
Joana Souza <souzajoana18@gmail.com>
2016-11-13 13:00:55 -0300

    removed unusable for loop and parse function, also fixed css selectors

--- dailyspider/dailyspider/spiders/globo_spider.py
+++ dailyspider/dailyspider/spiders/globo_spider.py
@@ -7,30 +7,28 @@ class GloboSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'http://g1.globo.com/economia/noticia/2016/11/pib-da-regiao-norte-deve-ter-o-maior-crescimento-em-2017-diz-estudo.html',
+            'http://g1.globo.com/politica/noticia/2016/11/stf-manda-uniao-depositar-em-juizo-cota-da-repatriacao-de-mais-16-estados.html',
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.scraping_daily)
+
+    #funcao para visitar paginas seguintesS
 
             
     def scraping_daily(self, response):
-        for d in response.css("glb-conteudo"):
-            yield {
-            'title': response.css('h1.entry-title::text').extract(),
-            #'date':response.css('published::text').extract(),
-            #'update':response.css('updated::text').extract(),
-            #'author':response.css('vcard::text').extract(),
-            #'content':response.css('materia-letra::text').extract()
+        xpath_clear = ".//*[not(self::script or self::style)]/text()[normalize-space(.)]" 
+        #for d in response.css("glb-conteudo"):
+        yield {
+            'title': response.css('h1.entry-title').xpath("string(.)").extract_first(),
+            'date':response.css('.materia-cabecalho .published::text').extract_first(),
+            'update':response.css('.materia-cabecalho .updated::text').extract_first(),
+            'author':response.css('.materia-assinatura .vcard').xpath("string(.)").extract_first(),
+            'content': ' '.join(response.css('.materia-conteudo').xpath(xpath_clear).extract()) #retorna texto sem as tags html
+
+            #response.css('.materia-conteudo::text').xpath(".").extract_first() 
+            
             }
 
+
+
     
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'globo-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-
-'''
-
-
-'''
